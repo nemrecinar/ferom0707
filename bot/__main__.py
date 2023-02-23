6822b03a78639c3e836f50637b5d25a36b0cafc6
nemrecinar <42311953+nemrecinar@users.noreply.github.com>
2023-02-23 23:11:22 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,9 +64,9 @@ async def start(client, message):
         start_string = 'Bot Started.\n' \
                     'Now I will send your files or links here.\n'
     else:
-        start_string = '🌹 Welcome To One Of A Modified Anasty Mirror Bot\n' \
-                    'This bot can Mirror all your links To Google Drive!\n' \
-                    '👨🏽‍💻 Powered By: @JMDKH_Team'
+        start_string = '🌹' \
+                    '' \
+                    ''
     await sendMessage(message, start_string)
 
 async def restart(client, message):
